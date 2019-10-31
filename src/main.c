2d3e646737aecd8647fd44d925658a555cd6b08a
Lucas <lucas.haug@hotmail.com>
2019-10-31 18:24:03 -0300

    :alien: Update due to lib update

--- src/main.c
+++ src/main.c
@@ -41,25 +41,27 @@ int main(void) {
     led_control(LED_SHIELD, LED_SET);
     HAL_Delay(1000);
 
-    rf24_t rf24 = rf24_get_default_config();
-    rf24.hspi = &hspi1,
-    rf24.csn_port = GPIOC,
-    rf24.csn_pin = GPIO_PIN_7,
+    rf24_dev_t* p_dev;
+    rf24_dev_t device = rf24_get_default_config();
+    p_dev = &device;
+    p_dev->platform_setup.hspi = &hspi1,
+    p_dev->platform_setup.csn_port = GPIOC,
+    p_dev->platform_setup.csn_pin = GPIO_PIN_7,
 
-    rf24.ce_port = GPIOA,
-    rf24.ce_pin = GPIO_PIN_9,
+    p_dev->platform_setup.ce_port = GPIOA,
+    p_dev->platform_setup.ce_pin = GPIO_PIN_9,
 
-    rf24.irq_port = GPIOB,
-    rf24.irq_pin = GPIO_PIN_6,
+    p_dev->platform_setup.irq_port = GPIOB,
+    p_dev->platform_setup.irq_pin = GPIO_PIN_6,
 
-    rf24.payload_size = PAYLOAD_SIZE,
+    p_dev->payload_size = PAYLOAD_SIZE,
 
     HAL_Delay(10);
 
     printf("================ NRF24 TEST ================\r\n");
     printf("Calling init [....]");
 
-    if (rf24_init(&rf24)) {
+    if (rf24_init(p_dev)) {
         // Deixou praticamente no deafult, só mudou em CONFIG CRCO
         printf("\b\b\b\b\b\b[ OK ]\r\n");
 
@@ -75,37 +77,37 @@ int main(void) {
             ;
     }
 
-    rf24_dump_registers(&rf24);
+    rf24_dump_registers(p_dev);
 
     // A partir daq vou configurar como ta no GettingStarted_HandlingData.ino
 
-    rf24_set_output_power(&rf24, RF24_12_dBm);
+    rf24_set_output_power(p_dev, RF24_12_dBm);
 
 #if (IS_RECEIVER == 1)
-    rf24_open_writing_pipe(&rf24, addresses[0]);
-    rf24_open_reading_pipe(&rf24, 1, addresses[1]);
+    rf24_open_writing_pipe(p_dev, addresses[0]);
+    rf24_open_reading_pipe(p_dev, 1, addresses[1]);
 #else
-    rf24_open_writing_pipe(&rf24, addresses[1]);
-    rf24_open_reading_pipe(&rf24, 1, addresses[0]);
+    rf24_open_writing_pipe(p_dev, addresses[1]);
+    rf24_open_reading_pipe(p_dev, 1, addresses[0]);
 #endif
 
-    rf24_dump_registers(&rf24);
+    rf24_dump_registers(p_dev);
 
 #if (IS_RECEIVER == 1)
-    rf24_start_listening(&rf24);
+    rf24_start_listening(p_dev);
 #endif
 
     for (;;) {
 #if (IS_RECEIVER == 1)
 
-        if (rf24_available(&rf24, NULL)) {
-            while (rf24_available(&rf24, NULL)) {
-                rf24_read(&rf24, buffer, rf24.payload_size);
+        if (rf24_available(p_dev, NULL)) {
+            while (rf24_available(p_dev, NULL)) {
+                rf24_read(p_dev, buffer, p_dev->payload_size);
             }
 
             printf("Recebendo: ");
 
-            for (int i = 0; i < rf24.payload_size; i++) {
+            for (int i = 0; i < p_dev->payload_size; i++) {
                 if (i == 12) {
                     printf(" no. %d", (int) buffer[i]);
                 } else {
@@ -123,7 +125,7 @@ int main(void) {
 
         printf("\r\n");
 
-        if (rf24_write(&rf24, buffer, PAYLOAD_SIZE, true)) {
+        if (rf24_write(p_dev, buffer, PAYLOAD_SIZE, true)) {
             printf("Virtual Hug Sent!\r\n");
         }
 
@@ -131,7 +133,7 @@ int main(void) {
         (buffer[12])++;
 #endif
 
-        rf24_print_status(&rf24);
+        rf24_print_status(p_dev);
         printf("\r\n");
         HAL_Delay(500);
     }
